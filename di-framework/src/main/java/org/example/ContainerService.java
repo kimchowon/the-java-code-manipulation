80810aeba9445b9eee043b39a3c345801a7be070
김초원 <zzzcc33@naver.com>
2022-02-23 22:37:06 +0900

    DI Framework

--- di-framework/src/main/java/org/example/ContainerService.java
+++ di-framework/src/main/java/org/example/ContainerService.java
@@ -6,15 +6,24 @@ import java.util.Arrays;
 public class ContainerService {
 
     public static <T> T getObject(Class<T> classType) {
+        // 1.인스턴스 생성
         T instance = createInstance(classType);
+
+        // 2.인스턴스의 필드들 조회
         Arrays.stream(classType.getDeclaredFields()).forEach(f -> {
+
+            // 3.필드에 붙어있는 @Inject 어노테이션을 조회
             Inject annotation = f.getAnnotation(Inject.class);
+
+            // 4.Inject 어노테이션이 붙어있다면
             if (annotation != null) {
                 Class<?> type = f.getType();
 
+                // 5.필드의 클래스 타입으로 인스턴스 생성
                 Object fieldInstance = createInstance(type);
                 f.setAccessible(true);
                 try {
+                    // 필드에 생성한 인스턴스를 주입.
                     f.set(instance, fieldInstance);
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException(e);
@@ -27,6 +36,7 @@ public class ContainerService {
     public static <T> T createInstance(Class<T> classType) {
 
         try {
+            // 해당 타입인 클래스의 디폴트 생성자로 인스턴스 생성
             return classType.getConstructor(null).newInstance();
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException(e);
--- di-framework/src/test/java/org/example/ContainerServiceTest.java
+++ di-framework/src/test/java/org/example/ContainerServiceTest.java
@@ -1,8 +1,21 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ContainerServiceTest {
 
+    @Test
+    public void bookRepository_getObject() {
+        BookRepository bookRepository = ContainerService.getObject(BookRepository.class);
+        assertNotNull(bookRepository);
+    }
 
+    @Test
+    public void bookServ_getObject() {
+        BookService bookService = ContainerService.getObject(BookService.class);
+        assertNotNull(bookService);
+        assertNotNull(bookService.bookRepository);
+    }
 }
