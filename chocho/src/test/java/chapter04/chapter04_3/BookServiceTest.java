4b6f80ca9cab40334e69827c8ea1e42706c21ef7
김초원 <zzzcc33@naver.com>
2022-02-27 14:25:15 +0900

    클래스만 있을 때 프록시 만들기 1. cglib 2. bytebuddy

--- chocho/src/test/java/chapter04/chapter04_3/BookServiceTest.java
+++ chocho/src/test/java/chapter04/chapter04_3/BookServiceTest.java
@@ -4,13 +4,17 @@ import chapter04.chapter04_1.Book;
 import chapter04.chapter04_1.BookService;
 import chapter04.chapter04_1.BookServiceImpl;
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.InvocationHandlerAdapter;
 import org.junit.jupiter.api.Test;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
 
 class BookServiceTest {
 
@@ -53,6 +57,15 @@ class BookServiceTest {
     public void di_bytebuddy() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<? extends chapter04.chapter04_3.BookService> proxyClass = new ByteBuddy()
                 .subclass(chapter04.chapter04_3.BookService.class)
+                .method(named("rent")).intercept(InvocationHandlerAdapter.of(new InvocationHandler() {
+
+                    chapter04.chapter04_3.BookService bookService = new chapter04.chapter04_3.BookService();
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        return method.invoke(bookService, args);
+                    }
+
+                }))
                 .make()
                 .load(BookService.class.getClassLoader())
                 .getLoaded();
