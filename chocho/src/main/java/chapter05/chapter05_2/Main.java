f65183cc056d035507f5ddb4e4328fb67bf8c0a4
김초원 <zzzcc33@naver.com>
2022-03-03 18:43:57 +0900

    annotation processing

--- chocho/src/main/java/chapter05/chapter05_2/Main.java
+++ chocho/src/main/java/chapter05/chapter05_2/Main.java
@@ -4,4 +4,9 @@ package chapter05.chapter05_2;
  * annotation-processor-project 참조
  */
 public class Main {
+
+    public static void main(String[] args) {
+        MyMoja myMoja = new MyMoja();
+        System.out.println("Hello World");
+    }
 }
---
+++ chocho/src/main/java/chapter05/chapter05_2/MyMoja.java
@@ -0,0 +1,7 @@
+package chapter05.chapter05_2;
+
+import me.chocho.Magic;
+
+@Magic
+public class MyMoja {
+}
